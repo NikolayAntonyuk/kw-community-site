37d71823941476fc8de1514898a3a318d0d9d160
Mykola Antoniuk <mykola.antoniyk@gmail.com>
2026-08-18 19:17:41 -0400

    chore: remove temporary update scripts

--- update_ids.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('data/specialists.json', 'utf8'));
-let id = 1;
-for (const item of data) {
-    item.id = id++;
-}
-fs.writeFileSync('data/specialists.json', JSON.stringify(data, null, 2));
